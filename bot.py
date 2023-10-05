facfeac48c5f3d3b301173f23e7f4cb0e0482756
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-10-05 13:07:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,16 +4,16 @@ from aiohttp import web
 from route import web_server
 
 class Bot(Client):
-    def __init__(self):
-        super().__init__(
-            name="renamer",
+
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=200,
             plugins={"root": "plugins"},
             sleep_threshold=15,
-	@@ -20,27 +23,23 @@ async def start(self):
+        )
+
+    async def start(self):
         await super().start()
         me = await self.get_me()
         self.mention = me.mention
