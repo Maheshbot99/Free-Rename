0fe67905cf3c4b96e342f0743d12950997454e1e
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2024-08-10 19:37:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ API_ID = os.environ.get("API_ID", "22525529")
 
 API_HASH = os.environ.get("API_HASH", "840111f82bbd1d2d3de5055afccf6a92")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5355979778:AAE-E9XXHb5Lefzlgdh6nDUbFDMIdEUyCVU") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6769970206:AAFQR5sZYbwxuWn-PVbVEqARHpSYZ0_nvJQ") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "CrazyXBots") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "Prime_SpoT") 
 
 DB_NAME = os.environ.get("DB_NAME","Name")     
 
@@ -18,6 +18,6 @@ FLOOD = int(os.environ.get("FLOOD", "90"))
 
 START_PIC = os.environ.get("START_PIC", "https://graph.org/file/875bc8c833cfa79f4c272.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5450011131').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '7298415492').split()]
 
 PORT = os.environ.get('PORT', '8080')
